e82ff1bb5da7fff91d696e4f57456b916e8fc06a
riderexmachina <riderexmachina@protonmail.com>
2019-10-02 18:31:05 -0700

    Add necessary features
    Removed trailing comma so JSON files can actually be parsed, added parsing of JSON files, made some CSS changes
    
    Signed-off-by: riderexmachina <riderexmachina@protonmail.com>

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -7,8 +7,10 @@ var ppaSupport = false;
 var companySupported = false;
 
 function displayResults(distro) {
-    distro = distro.toLowerCase().replace(/^a-z/)
-    var results = $.getJSON("../json/" + distro +".json", function(data) { document.getElementById("content").innerHTML = data; });
+    distro = distro.toLowerCase()
+    var results = $.getJSON("../json/" + distro +".json");
+    var info = JSON.parse(results);
+    document.getElementById("content").innerHTML = "We recommend <span id='recommended'>" + info.name + "</span><br /><a href='" + info.download_link +"' class='dl-link'>Download it here</a><br /><br />Information<br/>" + info.description + "<br /><br /><span id='more-info'><a href='"+ info.linux_delta +"'>Linux Delta Rating</a> | <a href='"+ info.distro_test +"'>Try it out</a> | <a href='" + info.distro_watch + "'>Distro Watch Page</a></span>";
 }
 
 function windows() {
