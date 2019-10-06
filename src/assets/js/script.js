bceb2cb62e173432adf529a3af2d85f01431e871
riderexmachina <riderexmachina@protonmail.com>
2019-10-06 14:26:13 -0700

    Added Zorin, made some changes to the logic, other small things
    *Note: This does break the first question at the moment. This will be fixed in the next commit"
    
    Signed-off-by: riderexmachina <riderexmachina@protonmail.com>

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -11,15 +11,41 @@ function displayResults(distro) {
     distro = distro.toLowerCase();
     file = "./assets/json/" + distro +".json";
     console.log(file)
-    var results = $.getJSON(file, function(info ) { 
-        document.getElementById("content").innerHTML = "We recommend <span id='recommended'>" + info.name + "</span><br /><a href='" + info.download_link +"' class='dl-link'>Download it here</a><br /><br />Information<br/>" + info.description + "<br /><br /><span id='more-info'><a href='"+ info.linux_delta +"'>Linux Delta Rating</a> | <a href='"+ info.distro_test +"'>Try it out</a> | <a href='" + info.distro_watch + "'>Distro Watch Page</a></span>";
+    var results = $.getJSON(file, function(info) { 
+        document.getElementById("content").innerHTML = `
+        We recommend <span id='recommended'>` + info.name + `</span>
+        <br />
+        <a href='` + info.download_link +`' class='dl-link'>Download it here</a>
+        <br />
+        Desktop Environments:
+        <br />
+        <b>Information:</b>
+        <br/>`
+         + info.description + 
+         `<br /><br /><span id='more-info'>
+         <a href='`+ info.linux_delta +`'>Linux Delta Rating</a> | 
+         <a href='`+ info.distro_test +`'>Try it out</a> | 
+         <a href='` + info.distro_watch + `'>Distro Watch Page</a></span>`;
      });
-    //var info = results;
-    //console.log(info);
     
 }
 
-function windows() {
+function desktopType(desktop) {
+    if (desktop == "windows") {
+        windowsLike = true;
+        console.log("Windows-like");
+    }
+    else if (desktop == "mac") {
+        macLike = true;
+        console.log("Mac-like");
+    }
+    else if (desktop == "whatever") {
+        windowsLike = true;
+        macLike = true;
+        console.log("\"Whatever\"")
+    }
+}
+/*function windows() {
     windowsLike = true;
     console.log("Windows-like")
     displayResults("Linux Mint")
@@ -34,10 +60,10 @@ function mac() {
 
 function whatever() {
     console.log("\"Whatever\"")
-    autoChoice()
-}
+    displayResults("Pop!_OS")
+}*/
 
-autoDistros = ["Kubuntu", "Pop!_OS", "elementaryOS", "Linux Mint"];
+autoDistros = ["Kubuntu", "Pop!_OS", "elementaryOS", "Linux Mint", "Zorin"];
 function autoChoice() {
     distro = autoDistros[Math.round(Math.random() * autoDistros.length)];
     if (distro == undefined) {
